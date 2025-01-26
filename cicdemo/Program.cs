05427a1687d53f67233df8969a8b318d0c14f472
Marco Castellani <marcocastellani@hotmail.com>
2025-01-26 17:15:08 +0100

    cors

--- cicdemo/Program.cs
+++ cicdemo/Program.cs
@@ -3,26 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("BlogDC") ?? throw new InvalidOperationException("Connection string 'BlogDC' not found.");
 
 builder.Services.AddDbContext<BlogDC>(options => options.UseNpgsql(connectionString));
-
+builder.Services.AddCors(p=>p.AddDefaultPolicy(b=>b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
 var app = builder.Build();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-app.UseCors(p=>p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 
+app.UseCors(p=>p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
-
-
